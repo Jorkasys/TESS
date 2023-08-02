979d38d8cd5f92f40ce662bb2d0958a1da5576cd
Jaight <artem.matrosov.06@mail.ru>
2023-08-02 12:07:35 +0300

    MY own branch

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def say_goodbb(joke):
+    print(f'See ya {joke}')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    say_goodbb('boba')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
